8dc2815985391a4df36c0fbd06bd4f5334410bf6
debora-gross <debygross@gmail.com>
2022-09-27 18:03:39 +0100

    Components and Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //esse app-root é o componente que deve ser usado como tag no index: <app-root></app-root>
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'MyProject';
+  
 }
